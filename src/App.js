8cedb54baf0576db062783f436930c1ca678c8fb
FdgCertainly <franco.giuntoli@gmail.com>
2023-06-19 00:14:44 +0200

    added vercel/analytics

--- src/App.js
+++ src/App.js
@@ -1,39 +1,58 @@
-import {useState} from 'react'
-import { Container, Stack, Button } from "react-bootstrap";
-import { usePopups } from "./context/PopupsContext";
+import { useState } from "react"
+import { Container, Stack, Button } from "react-bootstrap"
+import { usePopups } from "./context/PopupsContext"
 
-import AddPopupModal from "./component/AddPopupModal";
-import PopupCard from "./component/PopupCard";
+import AddPopupModal from "./component/AddPopupModal"
+import PopupCard from "./component/PopupCard"
+import { Analytics } from "@vercel/analytics/react"
 
-import './App.css';
+import "./App.css"
 
 function App() {
- const [showAddPopupModal, setShowAddPopupModal] = useState(false)
+  const [showAddPopupModal, setShowAddPopupModal] = useState(false)
 
-const {getPopups, deletePopup} = usePopups()
-function handleAddPopupModal(e){
-  e.target.blur()
-  setShowAddPopupModal(true)
-}
+  const { getPopups, deletePopup } = usePopups()
+  function handleAddPopupModal(e) {
+    e.target.blur()
+    setShowAddPopupModal(true)
+  }
 
   return (
     <>
-    <Container style={{ width:"60%", maxWidth:"600px"}}>
-      <Stack direction="horizontal" gap='2' className='mb-4'>
-        <h1 className="me-auto font-face-gt">Popups</h1>
-        <Button variant='danger' onClick={(e)=>{handleAddPopupModal(e)}}>Add Popup</Button>
-      </Stack>
-      <div style={{display:'grid', gridTemplateColumns:'repeat(auto-fill, mixmax(300px,1fr))', gap:'1rem', alignItems:'flex-start'}}>
-        {
-        getPopups.map(data=>(
-          <PopupCard key={data.ref} data={data} deletePopup={deletePopup} />
-        ))
-        }
-      </div>
-    </Container>
-    <AddPopupModal show={showAddPopupModal} handleClose={()=>{setShowAddPopupModal(false)}}/>
-  </>
-  );
+      <Container style={{ width: "60%", maxWidth: "600px" }}>
+        <Stack direction="horizontal" gap="2" className="mb-4">
+          <h1 className="me-auto font-face-gt">Popups</h1>
+          <Button
+            variant="danger"
+            onClick={(e) => {
+              handleAddPopupModal(e)
+            }}
+          >
+            Add Popup
+          </Button>
+        </Stack>
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fill, mixmax(300px,1fr))",
+            gap: "1rem",
+            alignItems: "flex-start",
+          }}
+        >
+          {getPopups.map((data) => (
+            <PopupCard key={data.ref} data={data} deletePopup={deletePopup} />
+          ))}
+        </div>
+      </Container>
+      <AddPopupModal
+        show={showAddPopupModal}
+        handleClose={() => {
+          setShowAddPopupModal(false)
+        }}
+      />
+      <Analytics mode={"production"} />;
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
-import './index.css';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css"
+import "./index.css"
 
-import { PopupsProvider } from './context/PopupsContext';
+import { PopupsProvider } from "./context/PopupsContext"
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
     <PopupsProvider>
       <App />
     </PopupsProvider>
   </React.StrictMode>
-);
+)
